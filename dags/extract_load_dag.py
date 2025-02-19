229728e3de5573566d6590efdf4a866bee9b42eb
Carvalhooneto <aderaldo.carvalho@dcx.ufpb.br>
2025-02-19 16:18:17 -0300

    update dag

--- dags/extract_load_dag.py
+++ dags/extract_load_dag.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.decorators import task
-from extract import extract_from_api
-from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.models.baseoperator import chain
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from extract import extract_from_api
+# Caminhos dentro do container, conforme o mapeamento dos volumes no Docker
+DBT_PROJECT_DIR = "/dbt_project"  # Diretório mapeado para dentro do container
+DBT_PROFILES_DIR = "/root/.dbt"   # Diretório mapeado para dentro do container
 
 with DAG(
         'extract_load_dag',
@@ -13,11 +17,41 @@ with DAG(
         start_date=datetime(2024, 10, 1, 0, 0),
 ) as dag:
     
+    create_schemas = SQLExecuteQueryOperator(
+        task_id = "create_schemas",
+        sql = "sql/create_schemas.sql",
+        conn_id = "pg_conn"
+    )
+
     @task
     def extract_load_agregados_censo(postgres_conn_id):
         db_conn = PostgresHook(postgres_conn_id).get_sqlalchemy_engine()
         extract_from_api.extract_load_from_zip(db_conn, table="despesas_contratadas", schema="sa_eleicoes")
+    
+    
+    create_dim_tables = SQLExecuteQueryOperator(
+        task_id = "create_dim_tables",
+        sql = "sql/create_dim_tables.sql",
+        conn_id = "pg_conn"
+    )
+
+    create_fact_table = SQLExecuteQueryOperator(
+        task_id = "create_fact_table",
+        sql = "sql/create_fact_table.sql",
+        conn_id = "pg_conn"
+    )
+    insert_dim_data = SQLExecuteQueryOperator(
+        task_id = "insert_dim_data",
+        sql = "sql/insert_dim_data.sql",
+        conn_id = "pg_conn"
+    )
+    insert_fact_data = SQLExecuteQueryOperator(
+        task_id = "insert_fact_data",
+        sql = "sql/insert_fact_data.sql",
+        conn_id = "pg_conn"
+    )
 
     extract_load = extract_load_agregados_censo("pg_conn")
 
-    chain(extract_load)
+    
+    chain(create_schemas, extract_load, create_dim_tables, create_fact_table, insert_dim_data, insert_fact_data)
